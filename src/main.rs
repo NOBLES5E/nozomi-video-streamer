0eebe0f1c14cb375ed64a415d944de63912e6ca4
Xiangru Lian <admin@mail.xrlian.com>
2020-02-02 02:47:21 -0800

    use boxedfilters

--- src/main.rs
+++ src/main.rs
@@ -203,14 +203,15 @@ mod filters {
     use std::convert::Infallible;
     use warp::filters::path::FullPath;
     use std::path::PathBuf;
+    use warp::filters::BoxedFilter;
 
-    pub fn with_shared_info(db: SharedAppData) -> impl Filter<Extract=(SharedAppData, ), Error=Infallible> + Clone {
+    pub fn with_shared_info(db: SharedAppData) -> BoxedFilter<(SharedAppData,)> {
         warp::any().map(move || {
             db.clone()
-        })
+        }).boxed()
     }
 
-    pub fn is_dir(db: SharedAppData) -> impl Filter<Extract=(), Error=Rejection> + Clone {
+    pub fn is_dir(db: SharedAppData) -> BoxedFilter<()> {
         warp::path::full().and(with_shared_info(db)).and_then(
             async move |path: FullPath, data: SharedAppData| {
                 let path: PathBuf = percent_encoding::percent_decode_str(&path.as_str()[1..]).decode_utf8().expect("cannot decode url").parse()?;
@@ -220,10 +221,10 @@ mod filters {
                     false => Err(warp::reject::reject()),
                 }
             }
-        ).untuple_one()
+        ).untuple_one().boxed()
     }
 
-    pub fn is_file(db: SharedAppData) -> impl Filter<Extract=(), Error=Rejection> + Clone {
+    pub fn is_file(db: SharedAppData) -> BoxedFilter<()> {
         warp::path::full().and(with_shared_info(db)).and_then(
             async move |path: FullPath, data: SharedAppData| {
                 let path: PathBuf = percent_encoding::percent_decode_str(&path.as_str()[1..]).decode_utf8().expect("cannot decode url").parse()?;
@@ -234,7 +235,7 @@ mod filters {
                     false => Err(warp::reject::reject()),
                 }
             }
-        ).untuple_one()
+        ).untuple_one().boxed()
     }
 }
 
