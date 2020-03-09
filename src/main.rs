895a1f21e23ebb426a5768ee69a5c2db275223bd
Xiangru Lian <admin@mail.xrlian.com>
2020-03-08 23:02:55 -0700

    fix url

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(async_closure)]
+#![feature(str_strip)]
 
 use anyhow::Result;
 use std::io::Write;
@@ -162,9 +163,10 @@ async fn serve_dir(path: FullPath, data: SharedAppData) -> Result<impl warp::Rep
             |entry| {
                 let filename = entry.expect("cannot read file").file_name().to_str().unwrap().to_owned();
                 log::info!("filename: {:?}", filename);
+                let url = path.join(filename).to_str().unwrap();
                 DirectoryFile {
                     filename: filename.clone(),
-                    url: filename.clone(),
+                    url: "/".to_string() + url.strip_prefix("/").unwrap_or(url),
                 }
             }
         ).collect(),
