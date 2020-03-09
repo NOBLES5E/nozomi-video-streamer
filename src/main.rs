2edf4157ace61f153daac9ee160fa3607b0fa208
Xiangru Lian <admin@mail.xrlian.com>
2020-03-08 21:12:50 -0700

    Log url path in the server side.

--- src/main.rs
+++ src/main.rs
@@ -216,6 +216,7 @@ mod filters {
             async move |path: FullPath, data: SharedAppData| {
                 let path: PathBuf = percent_encoding::percent_decode_str(&path.as_str()[1..]).decode_utf8().expect("cannot decode url").parse()?;
                 let realpath = data.lock().unwrap().serving_dir.join(&path);
+                log::info!("realpath {:?}", realpath);
                 match realpath.is_dir() {
                     true => Ok(()),
                     false => Err(warp::reject::reject()),
